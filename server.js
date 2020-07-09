916f3ffa29990bb610a208d753f7896c5822869e
Lucas Godoi <godoi.lucas@hotmail.com>
2020-07-09 20:53:55 -0300

    sequelize init

--- server.js
+++ server.js
@@ -1,16 +1,5 @@
 const app = require("./src/config/custom-express");
-const conexao = require('./src/config/conexao');
-const Tabelas  = require('./src/config/tabelas');
 
-conexao.connect(erro => {
-    if(erro){
-        console.log(erro);
-    } else{
-        console.log("MySQL conectado com sucesso!");
-        
-        const tabelas = new Tabelas(conexao);
-        
-        const port = process.env.PORT | 3000;
-        app.listen(port,() => console.log(`Servidor rodando na porta ${port}`));
-    }
-});
+
+const port = process.env.PORT | 3000;
+app.listen(port,() => console.log(`Servidor rodando na porta ${port}`));
--- src/app/controllers/RevendedorController.js
+++ src/app/controllers/RevendedorController.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 class RevendedorController{
     login(req, res){
         res.status(200).json("Login");
@@ -16,7 +18,17 @@ class RevendedorController{
     }
 
     exibeCashbackAcumulado(req, res){
-        res.status(200).json("Exibe Cashback Acumulado");
+        try {
+            fetch('https://mdaqk8ek5j.execute-api.us-east-1.amazonaws.com/v1/cashback?cpf=12312312323', {
+                method: 'GET',
+                headers: { token: 'ZXPURQOARHiMc6Y0flhRC1LVlZQVFRnm' }
+            })
+            .then(r => r.json())
+            .then(json => res.status(200).json(json.body));
+        } catch (error) {
+            res.status(200).json({ erro: error.message });
+        }
+            
     }
 }
 
